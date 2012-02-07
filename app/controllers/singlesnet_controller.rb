f03f7165ca212e3cfa7a30204843f702470bc82a
Nick Coelius <coelius@okcupidlabs.com>
2012-02-06 17:40:31 -0800

    Full profile submission, auto-no fields

--- app/controllers/singlesnet_controller.rb
+++ app/controllers/singlesnet_controller.rb
@@ -3,7 +3,7 @@ class SinglesnetController < ApplicationController
   API_KEY = "b53671a74a41b3e513e0abf4fdedf3af"
   CID = "10184"
   SINGLESNET_BASE_URL= "http://api.singlesnet.com/api/1.2/signup/submit"
-  SINGLESNET_CORE_URL= "/signup/submit/core" + "?key=" + Rails.configuration.singlesnet_api_key + "&format=xml&campaign_id=" + CID
+  SINGLESNET_CORE_URL= "/signup/submit" + "?key=" + Rails.configuration.singlesnet_api_key + "&format=xml&campaign_id=" + CID
   
   
   attr_reader :errors
@@ -28,19 +28,35 @@ class SinglesnetController < ApplicationController
       "zipcode" => @user.zipcode.to_s,
       "captcha_key" => @user.captcha_key,
       "captcha_answer" => @user.captcha_answer,
-      "city" => @user.city_id
-      # #Nothing below here is required.  Why are we displaying it.
-      #       "height" => @user.height,
-      #       "body" => @user.body,
-      #       "hair" => @user.hair,
-      #       "eye" => @user.eye,
-      #       "race" => @user.race,
-      #       "occupation" => @user.occupation,
-      #       "drinking" => @user.drinking,
-      #       "smoking" => @user.smoking,
-      #       "about" => @user.about
+      "city" => @user.city_id,
+      "height" => @user.height,
+      "bodytype" => @user.body,
+      "hair_color" => @user.hair,
+      "eye_color" => @user.eye,
+      "race" => @user.race,
+      "occupation" => @user.occupation,
+      "drink" => @user.drinking,
+      "smoke" => @user.smoking,
+      "personal_description_wanted" => @user.about,
+      "sports_play" => "no",
+      "exercise" => "no",
+      "outdoor" => "no",
+      "artistic" => "no",
+      "religion" => "no",
+      "sports_fan" => "no",
+      "pet" => "no",
+      "race_date" => "no",
+      "country_date" => "no",
+      "diet" => "no",
+      "relationship_serious" => "no",
+      "children" => "no",
+      "children_date" => "no",
+      "high_school" => "no",
+      "college" => "no",
     }
     @response = poster.class.post(SINGLESNET_CORE_URL, :body => hash)
-    # p @response
+    # core_key = Nokogiri::XML.parse(@response.body).css("success").attribute("core_key").value
+    p @response
+    p @user.city_id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   def city_finder
+    p zipcode
     SinglesnetApi.find_city(zipcode)
   end
   
