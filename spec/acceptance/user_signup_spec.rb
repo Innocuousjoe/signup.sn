371de677d7aceb80c072d105590b97f63ddb5f21
Nick Coelius <coelius@okcupidlabs.com>
2012-02-14 15:46:00 -0800

    error path for page 2

--- spec/acceptance/user_signup_spec.rb
+++ spec/acceptance/user_signup_spec.rb
@@ -8,7 +8,7 @@ def do_first_form opts={}
     fill_in "user_username", with: opts[:username] || "OKCW999"
     fill_in "user_password", with: "password"
     fill_in "user_email", with: opts[:email] || "OKCW999@sharklasers.com"
-    fill_in "user_captcha_answer", with: "pArADE"
+    fill_in "user_captcha_answer", with: opts[:captcha] || "pArADE"
     click_button "user_submit"
   end
 end
@@ -45,17 +45,17 @@ feature "Users", "I want to be able to get past the second form" do
     end
   end
   
-  # scenario "When there are errors", :js => true do
-  #   VCR.use_cassette('new_user_error') do
-  #     visit "/users/new"
-  #     # click("#content")
-  #     fill_in "user_zipcode", with: "94110"
-  #     # fill_in "user_username", with: "" # this is blank!  terrible error!
-  #     fill_in "user_password", with: "password"
-  #     # fill_in "user_email", with: "OKCW1999@sharklasers.com" # Also a grievous error!
-  #     fill_in "user_captcha_answer", with: "buNts"
-  #     click_button "user_submit"
-  #     page.should have_content "is invalid"
-  #   end
-  # end
+  scenario "When there are no errors", :js => true do
+    do_first_form(:vcr => 'new_user_error_two', :email => "OKCW1111@sharklasers.com", :username => "OKC1111", :captcha => "dabbED")
+    VCR.use_cassette('new_user_part_two_with_errors') do
+      page.execute_script('$("select").selectBox("destroy")') # DIE SELECTBOX DIE.  The jquery plugin was irritating me SO I DESTROYED IT
+      select "White / Caucasian", :from => "user_race"
+      select "Dark brown", :from => "user_hair"
+      select "Fit / Athletic", :from => "user_body"
+      select "6'", :from => "user_height"
+      fill_in "user_about", :with => "This is super important text"
+      click_button "user_submit"
+      page.should have_content "What is your occupation?"
+    end
+  end
 end
